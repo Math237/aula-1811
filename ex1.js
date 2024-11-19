5bff82bc371fc5d17099634c473e08bb827bc26f
Math237 <richardgarcia28688@gmail.com>
2024-11-18 22:27:48 -0300

    aula 1811

---
+++ ex1.js
@@ -0,0 +1,33 @@
+/*
+Elabore um programa que contenha uma lista de produtos:
+	1 - Hamburguer - R$ 10
+	2 - X-Salada - R$ 15
+	3 - X-Bacon - R$ 20
+	
+	Solicite um código de produto, percorra a lista e exiba seus dados
+*/
+
+var menu  = [
+    {codigo: 1, nome: "Hamburger", valor: 10},
+    {codigo: 2, nome: "X-Salada", valor: 15},
+    {codigo: 3, nome: "X-Bacon", valor: 20},
+];
+
+console.log("Código | Nome             | Valor");
+for (var i = 0; i < menu.length; i++ ){
+    console.log( menu[i].codigo, "  |", menu[i].nome, "   |", menu[i].preco );
+}
+var codigo = parseInt(prompt("Escolha o código de um lanche:"));
+var achou = false;
+for(var i = 0; i < menu.length; i++){
+if (codigo == menu[i].codigo){
+console.log("\n\n ==== Escolhido foi: ====");
+console.log(menu[i].codigo, "   |", menu[i].nome, "    |",menu[i].preco );
+achou = true
+}
+
+}
+
+if(achou == false){
+    console.error("\n\n ==== Produto não encontrado!!! ====");
+}
